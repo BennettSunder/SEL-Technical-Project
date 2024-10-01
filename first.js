8fbfc28b46c6e863bc9a1cbcb7cef84e07a510e6
bennett <bennettsunder@gmail.com>
2024-10-01 00:44:13 -0400

    first commit
    got my home page and most of the first prompt

---
+++ first.js
@@ -0,0 +1,18 @@
+
+
+function addItem(){
+  let userInput = document.getElementById('input1').value;
+  // alert(textItem);
+  
+  const newLI = document.createElement('li');
+  newLI.textContent = userInput;
+  newLI.innerHTML += '<button onclick="kamikazi(this)">&#x2715;</button>';
+
+  document.getElementById('list').appendChild(newLI);
+  document.getElementById('input1').value = '';
+
+}
+
+function kamikazi(element){
+  element.parentElement.remove();
+}
