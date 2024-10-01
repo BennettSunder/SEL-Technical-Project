1d2b55c6c1e577491904a5caff8d59a47f880931
bennett <bennettsunder@gmail.com>
2024-10-01 08:10:16 -0400

    finished prompt 1
    pretty self explanitory

--- first.js
+++ first.js
@@ -4,7 +4,7 @@ function addItem(){
   let userInput = document.getElementById('input1').value;
   // alert(textItem);
   
-  const newLI = document.createElement('li');
+  const newLI = document.createElement('div');
   newLI.textContent = userInput;
   newLI.innerHTML += '<button onclick="kamikazi(this)">&#x2715;</button>';
 
